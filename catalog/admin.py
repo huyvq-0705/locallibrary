f58de9ef72bff74df16a5a7dc5db408c4abfc31d
Vũ Quang Huy <110976640+huyvq-0705@users.noreply.github.com>
2025-07-16 11:56:48 +0700

    Django Part 4: Django admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,60 @@
-from __future__ import annotations
-
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        'last_name',
+        'first_name',
+        'date_of_birth',
+        'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    ordering = ('last_name', 'first_name')
+    search_fields = ('first_name', 'last_name')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+    ordering = ('title',)
+    search_fields = ('title', 'author__last_name')
+
+    def display_genre(self, obj):
+        return ', '.join(genre.name for genre in obj.genre.all()[:3])
+    display_genre.short_description = 'Genre'
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    ordering = ('due_back',)
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
