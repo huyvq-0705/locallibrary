a349d9f95f5e0526b8df3861c137abd257afe37f
Vu_Qu_Huy <vuquanghuy0805@gmail.com>
2025-07-18 14:14:43 +0700

    Django Tutorial Part 7: Sessions framework

--- catalog/views.py
+++ catalog/views.py
@@ -7,18 +7,29 @@ from catalog.constants import LOAN_STATUS_AVAILABLE, LOAN_STATUS_ON_LOAN
 from catalog.constants import LOAN_STATUS
 
 
+from django.shortcuts import render
+from .models import Book, BookInstance, Author
+from .constants import LOAN_STATUS_AVAILABLE
+
+
 def index(request):
+
     num_books = Book.objects.count()
     num_instances = BookInstance.objects.count()
     num_instances_available = BookInstance.objects.filter(
-        status__exact=LOAN_STATUS_AVAILABLE).count()
+        status__exact=LOAN_STATUS_AVAILABLE
+    ).count()
     num_authors = Author.objects.count()
+
+    num_visits = request.session.get('num_visits', 0) + 1
+    request.session['num_visits'] = num_visits
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context)
