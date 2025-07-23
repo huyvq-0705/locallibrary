daffe7702ec3eb8b55f8c3630c236927fdde6381
Vũ Quang Huy <110976640+huyvq-0705@users.noreply.github.com>
2025-07-23 14:40:50 +0700

    Django Tutorial Part 12: Django web application security

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secureadmin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),  # <-- Gọi đến app catalog
     # <-- Chuyển / về /catalog/
     path('', RedirectView.as_view(url='catalog/')),
